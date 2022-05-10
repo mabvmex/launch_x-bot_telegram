99d6d2c7114c22a11e952c3790cea657757db65d
mabvmex <mabvmex@gmail.com>
2022-05-10 10:37:00 -0500

    server test #1: pass

--- test/utils/server.test.js
+++ test/utils/server.test.js
@@ -1,44 +1,15 @@
-// describe("Unit test for Server", () => {
-//     test("1.- Testing to see if Jest works", (done) => {
-        
-//         const app = "./../../app/lib/server.js";
-//         const supertest = require("supertest");
-//         const req = supertest(app);
-        
-        
-//         const res = req.get("/test");
+const request = require("supertest");
+const app = require("./../../app/lib/server");
 
-//         expect(res.status).toBe(200);
-//         // expect(res.body.message).toBe("PASS!!!");
-//         done();
-//     });
-// });
+describe("1.- GET /v1/explorer/node", () => {
+    test("La solicitud muestra un 'status code' 200", async () => {
+        const response = await request(app).get("/").send();
+        expect(response.statusCode).toBe(200);
+        
+    });
+    
+    test("", () => {
 
-// describe("Unit test for Server", () => {
-//     it("2.- Get '/v1/explorer", (done) => {
-//         request(app)
-//             .get("/v1/explorer/node")
-//             .expect(200)
-//             .expect((res) => {
-//                 expect(res.json).toBe("hola");
-//             })
-//             .end((err, res) => {
-//                 if (err) return done(err);
-//                 return done();
-//             });
-//     });
+    });
 
-// test("2.- GET /v1/explorer/mission", (done) => {
-//     request(app)
-//         .get("/launchX")
-//         .expect(200)
-//         .expect((res) => {
-//             console.log(res.text);
-//             expect(res.text).toBe("launchX");
-//         })
-//         .end((err, res) => {
-//             if (err) return done(err);
-//             return done;
-//         });
-// });
-// });
+});
