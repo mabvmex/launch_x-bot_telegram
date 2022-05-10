1dfdb21219a83dbd9ad4ace337f2677594107290
mabvmex <mabvmex@gmail.com>
2022-05-10 13:36:27 -0500

    Se crea la funcionalidad correctamente

--- app/lib/services/FizzbuzzService.js
+++ app/lib/services/FizzbuzzService.js
@@ -1,34 +1,32 @@
-/* 
-Get a list of the explorers in node, 
-  - if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, 
-  - if is just divisible by 5 set the property trcik and the value BUZZ, 
-  - if is just divisible by 3 set the property trick and the value FIZZ, 
-  - otherwise set the property trick and the score value.
-*/
-
 class FizzbuzzService {
     static applyValidationInExplorer(explorer) {
-    
         if (explorer.score % 3 !== 0 && explorer.score % 5 !== 0) {
             explorer.trick = explorer.score;
             return explorer.trick;
-        } 
-        else if (explorer.score % 3 === 0 && explorer.score % 5 === 0) {
+        } else if (explorer.score % 3 === 0 && explorer.score % 5 === 0) {
             explorer.trick = "FIZZBUZZ";
             return explorer.trick;
-        }
-
-        else if (explorer.score % 3 === 0) {
+        } else if (explorer.score % 3 === 0) {
             explorer.trick = "FIZZ";
             return explorer.trick;
-        } 
-    
-        else if (explorer.score % 5 === 0) {
+        } else if (explorer.score % 5 === 0) {
             explorer.trick = "BUZZ";
             return explorer.trick;
-        } 
+        }
     }
 
+    static applyValidationInNumber(number) {
+        if (number % 3 !== 0 && number % 5 !== 0) {
+            return number;    
+        } else if (number % 3 === 0 && number % 5 === 0) {
+            return "FIZZBUZZ";
+        } else if (number % 3 === 0) {
+            return "FIZZ";
+        } else if (number % 5 === 0) {
+            // number.trick = "BUZZ";
+            return "BUZZ";
+        }
+    }
 }
 
 module.exports = FizzbuzzService;
