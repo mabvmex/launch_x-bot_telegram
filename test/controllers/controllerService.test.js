853a055c1ff90b3b4a64cc4c982c30bcdf37c2ed
mabvmex <mabvmex@gmail.com>
2022-05-14 20:43:57 -0500

    Se agregan pruebas exitosas para ExplorerControllers > getExplorersByStack

--- test/controllers/controllerService.test.js
+++ test/controllers/controllerService.test.js
@@ -34,12 +34,20 @@ describe("Unit Test for controllerService", () => {
         expect(amountofExplorers).toBe(10);
     });
 
-    test("4.- getResultsInNumberOnly", () => {
+    test("4.- FIZZBUZZ > getResultsInNumberOnly", () => {
         const number = 15;
         const getResultWithNumber = ExplorerController.getResultInNumberOnly(
             number
         );
         expect(getResultWithNumber).toBe("FIZZBUZZ");
+    });
+
+    test("5.- FIZZBUZZ > getExplorerByStack", () => {
+        const stack = "javascript";
+        const getExplorers = ExplorerController.getExplorerByStack(
+            stack
+        );
+        expect(getExplorers[0]).toBe("ajolonauta1");
     });
 
 
